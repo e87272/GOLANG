93cd34b7d0f878bc2dd3c1afa8bd4dc8b995e100
minhsiu.tsai <minhsiu.tsai@gosmio.biz>
2019-04-23 15:41:21 +0800

    資料夾整理

---
+++ index.go
@@ -0,0 +1,54 @@
+package main
+import( "fmt"
+		"github.com/kataras/iris"
+		"github.com/kataras/iris/websocket"
+		"./im"
+		"./gm"
+)
+
+func main() {
+	
+    ws := websocket.New(websocket.Config{
+        ReadBufferSize:  1024,
+        WriteBufferSize: 1024,
+	})
+	
+    ws.OnConnection(handleConnection)
+
+    app := iris.New()
+    // register the server on an endpoint.
+    // see the inline javascript code in the websockets.html, this endpoint is used to connect to the server.
+    app.Get("/echo", ws.Handler())
+
+	app.StaticWeb("/", "./index.html")
+	
+    // serve the javascript built'n client-side library,
+    // see websockets.html script tags, this path is used.
+    app.Any("/iris-ws.js", func(ctx iris.Context) {
+        ctx.Write(websocket.ClientSource)
+	})
+    app.Get("/im.js", func(ctx iris.Context) {
+		ctx.ServeFile("im.js", false)
+	})
+	
+	app.Run(iris.Addr(":8080"))
+	
+	fmt.Printf("End\n")
+}
+
+func handleConnection(c websocket.Connection) {
+    // Read events from browser
+    c.On("gmsocket", func(msg string) {
+        // Print the message to the console, c.Context() is the iris's http context.
+		//fmt.Printf("%s sent: %s\n", c.Context().RemoteAddr(), msg)
+		//fmt.Printf("%+v\n", c.Context())
+        // Write message back to the client message owner:
+		gm.ReceiveCustomPacket(c,msg);
+		
+	})
+	
+    c.On("imsocket", func(msg string) {
+		//fmt.Printf("%s : %s\n", "ReceiveImPacket", msg)
+		im.ReceiveImPacket(c,msg);
+    })
+}
